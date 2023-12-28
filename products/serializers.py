58a0b70bcf4ec4007c2c2208b04d4396058cac6b
cinfacol <cinfacol@gmail.com>
2023-12-27 23:31:49 -0500

    cambios menores

--- products/serializers.py
+++ products/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import Product, ProductViews
 
-from categories.serializers import CategorySerializer
+# from categories.serializers import CategorySerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
