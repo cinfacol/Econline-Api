8071e22245de0b04c1cadbedf01c7bc757c49e77
cinfacol <cinfacol@gmail.com>
2024-01-22 16:21:09 -0500

    Integrar Brand app con el admin backend

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from .models import Product, ProductViews, Media
+from .models import Product, Brand, ProductViews, Media
 
 
 @admin.register(Media)
 class MediaAdmin(admin.ModelAdmin):
     list_display = ["id", "alt_text", "image", "product", "created_at", "default"]
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ["name"]
 
 
 class MediaInline(admin.TabularInline):
