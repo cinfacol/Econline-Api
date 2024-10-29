8acead2e38b09af558c888c426cf8ada8121f6a6
cinfacol <cinfacol@gmail.com>
2024-10-28 19:12:51 -0500

    cambio puerto nginx

--- config/settings.py
+++ config/settings.py
@@ -175,8 +175,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
+    "http://localhost:9090",
+    "http://127.0.0.1:9090",
     "http://127.0.0.1:3000",
 ]
 
@@ -185,8 +185,8 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
+    "http://localhost:9090",
+    "http://127.0.0.1:9090",
 ]
 
 PASSWORD_HASHERS = [
