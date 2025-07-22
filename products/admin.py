0abe7c9283a36b0e9327727776f9d45b8deb34ff
cinfacol <cinfacol@email.com>
2025-07-22 14:33:22 -0500

    Refactor ProductAdmin in admin.py to improve list display, filtering, and search functionality

--- products/admin.py
+++ products/admin.py
@@ -5,9 +5,20 @@ from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-
     list_display = [
         "id",
         "name",
+        "slug",
+        "ref_code",
+        "is_active",
+        "published_status",
+        "get_categories",
     ]
-    list_filter = ["name"]
+    list_filter = ["is_active", "published_status", "category"]
+    search_fields = ["name", "ref_code", "description"]
+    readonly_fields = ["slug", "ref_code"]
+
+    def get_categories(self, obj):
+        return ", ".join([cat.name for cat in obj.category.all()])
+
+    get_categories.short_description = "Categories"
